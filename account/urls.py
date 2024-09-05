f7418e04135a003ed9eb4dcd19890bbfae6cfa3b
Hikatsukayba <hikatsukayba@gmail.com>
2024-09-05 19:22:57 +0100

    Backend base part

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
+from .views import ClientViewSet
 
 
 route =routers.DefaultRouter()
-route.register(r'usres')
+route.register(r'client',ClientViewSet)
 
 urlpatterns = route.urls
