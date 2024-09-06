b91e827e28696d97e994dd5699b89f1924acf3c4
Hikatsukayba <hikatsukayba@gmail.com>
2024-09-06 23:22:37 +0100

    models for account module

--- account/models.py
+++ account/models.py
@@ -3,7 +3,21 @@ from django.db import models
 # Create your models here.
 
 class ClientModel(models.Model):
-    pass
+    full_name=models.CharField(max_length=100)
+    adress=models.CharField(max_length=255)
+    phone=models.CharField(max_length=255)
+
 class TailorModel(models.Model):
-    pass
+    full_name = models.CharField(max_length=100)
+    experience_years = models.IntegerField()
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    rating = models.FloatField(default=0.0)  # To store tailor ratings
+
+    def __str__(self):
+        return self.full_name
+    
+class SpecializeModel(models.Model):
+    name=models.CharField(max_length=255)
+    tailors=models.ManyToManyField(TailorModel)
 
